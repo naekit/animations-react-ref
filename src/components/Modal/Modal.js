e0c603987060bea5ede585a3d68389db15808655
naekit <mykyta.onishchuck@code.berlin>
2023-01-29 12:14:56 -0500

    added transition on component level

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,17 +1,46 @@
 import React from "react"
+import { Transition } from "react-transition-group"
 
 import "./Modal.css"
 
-const modal = (props) => {
-	const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"]
+const animationTiming = {
+	enter: 400,
+	exit: 1000,
+}
 
+const modal = (props) => {
 	return (
-		<div className={cssClasses.join(" ")}>
-			<h1>A Modal</h1>
-			<button className="Button" onClick={props.closed}>
-				Dismiss
-			</button>
-		</div>
+		<Transition
+			in={props.show}
+			timeout={animationTiming}
+			mountOnEnter
+			unmountOnExit
+			onEnter={() => console.log("onEnter")}
+			onEntering={() => console.log("onEntering")}
+			onEntered={() => console.log("onEntered")}
+			onExit={() => console.log("onExit")}
+			onExiting={() => console.log("onExiting")}
+			onExited={() => console.log("onExited")}
+		>
+			{(state) => {
+				const cssClasses = [
+					"Modal",
+					state === "entering"
+						? "ModalOpen"
+						: state === "exiting"
+						? "ModalClosed"
+						: null,
+				]
+				return (
+					<div className={cssClasses.join(" ")}>
+						<h1>A Modal</h1>
+						<button className="Button" onClick={props.closed}>
+							Dismiss
+						</button>
+					</div>
+				)
+			}}
+		</Transition>
 	)
 }
 
